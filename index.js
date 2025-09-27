b56a550172df76063cf3a06eb69ddf66926dedd3
Dede <dedenyengor09@gmail.com>
2025-09-27 14:53:31 +0000

    My second commit

--- index.js
+++ index.js
@@ -5,21 +5,72 @@ const app = express();
 const port = 3000;
 const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[randomIndex]);
+});
 
 //2. GET a specific joke
-
+app.get("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+})
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+})
 
 //4. POST a new joke
-
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+})
 //5. PUT a joke
+app.put("/jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id);
+  const replaceJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  };
+  const serachIndex= jokes.findIndex((joke) => joke.id === id);
+  jokes[serachIndex] = replaceJoke;
+  res.json(replaceJoke);
+});
 
 //6. PATCH a joke
-
+app.patch("/jokes/:id",(req,res)=> {
+  const id = parseInt(req.params.id);
+  const updateJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  };
+   const serachIndex= jokes.findIndex((joke) => joke.id === id);
+  jokes[serachIndex] = updateJoke;
+  res.json(updateJoke);
+});
 //7. DELETE Specific joke
+app.delete("/jokes/:id",(res,req) => {
+  const id = parseInt(req.params.id);
+
+
+})
+
 
 //8. DELETE All jokes
 
